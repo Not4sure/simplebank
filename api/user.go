a5388e4c742c755eca1c99c91cb6f05e519c1392
NotSure <juliy.maj@gmail.com>
2023-12-06 19:34:30 +0200

    fixed typos

--- api/user.go
+++ api/user.go
@@ -82,9 +82,9 @@ type loginUserRequest struct {
 type loginUserResponse struct {
 	SessionID             uuid.UUID    `json:"session_id"`
 	AccessToken           string       `json:"access_token"`
-	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at`
+	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
 	RefreshToken          string       `json:"refresh_token"`
-	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at`
+	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
 	User                  userResponse `json:"user"`
 }
 
