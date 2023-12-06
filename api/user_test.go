081aca0980e4ca10a3472d10b2669991e100e56e
NotSure <juliy.maj@gmail.com>
2023-12-06 11:44:29 +0200

    added token maker to server struct

--- api/user_test.go
+++ api/user_test.go
@@ -89,7 +89,7 @@ func TestCreateUser(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStabs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// Marshal body data to JSON
